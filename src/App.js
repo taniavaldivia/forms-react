dc7c0ee761800407971bdd753dd2e5d563907382
taniavaldivia <tania.valdiviag@gmail.com>
2022-06-20 15:33:44 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import FormInput from './components/FormInput.js';
+
+const App = () => {
+  return <div className="App">
+    <form>
+      <FormInput/>
+      <FormInput/>
+      <FormInput/>
+      <FormInput/>
+    </form>
+  </div>
+};
+
+export default App;
